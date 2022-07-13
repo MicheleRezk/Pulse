629d77000c3a1733d69a34db06a6321fa9e5e80b
DESKTOP-2J4MC7J\Michele George <michelerezk2015@gmail.com>
2022-07-13 23:41:42 +0200

    adding validation for dto

--- Pulse.Application/Features/Users/Dtos/Dtos.cs
+++ Pulse.Application/Features/Users/Dtos/Dtos.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Pulse.Application.Features.Users.Dtos
 {
-    public record CreateUserDto(string FirstName, string LastName, string Email);
-    public record UpdateUserDto(string FirstName, string LastName, string Email);
+    public record CreateUserDto([Required] string FirstName, [Required] string LastName, [Required][EmailAddress] string Email);
+    public record UpdateUserDto([Required] string FirstName, [Required] string LastName, [Required][EmailAddress] string Email);
     public record UserDto(Guid UserId,string FirstName, string LastName, string Email);
 }
