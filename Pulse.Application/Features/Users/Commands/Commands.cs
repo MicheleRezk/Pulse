5347f165de585129c11cd0439ea201c0364d4775
DESKTOP-2J4MC7J\Michele George <michelerezk2015@gmail.com>
2022-07-13 21:58:16 +0200

    adding Commands and Queries

---
+++ Pulse.Application/Features/Users/Commands/Commands.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Pulse.Core.Entities;
+using System;
+
+namespace Pulse.Application.Features.Users.Commands
+{
+    public record CreateUserCommand(User user) : IRequest;
+    public record UpdateUserCommand(User user) : IRequest;
+    public record DeleteUserCommand(Guid userId) : IRequest;
+}
---
+++ Pulse.Application/Features/Users/Queries/Queries.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Pulse.Core.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Pulse.Application.Features.Users.Queries
+{
+    public record GetAllUsersQuery : IRequest<IEnumerable<User>>;
+    public record GetUserByIdQuery(Guid userId) : IRequest<User>;
+}
